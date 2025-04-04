8982db504c8133e9345c8908ce6c7b2bc789881f
PranaviDevulapalli <pranavidevulapalli@gmail.com>
2025-04-04 19:20:33 +0530

    Fixed model path issue

--- src/api.py
+++ src/api.py
@@ -14,6 +14,16 @@ import pandas as pd
 
 import joblib
 import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "API is working!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
 # Correct path for Render deployment
 model_path = os.path.join(os.path.dirname(__file__), "models\packing_model.pkl")
